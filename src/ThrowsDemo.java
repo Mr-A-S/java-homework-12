9f2a9f36fbe23c4f90efb2456b3cb80fbe35eb62
Andrey Soloveyv <mr.andrew.solovev@gmail.com>
2025-11-21 01:37:56 +0300

    Выполнено задание 5-8: Генерация исключений и повторный ввод

---
+++ src/ThrowsDemo.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ThrowsDemo {
+    public static void main(String[] args) {
+        ThrowsDemo demo = new ThrowsDemo();
+        demo.getKey();
+    }
+
+    public void getKey() {
+        Scanner myScanner = new Scanner(System.in);
+        boolean valid = false;
+        while (!valid) {
+            try {
+                System.out.print("Enter Key: ");
+                String key = myScanner.next();
+                printDetails(key);
+                valid = true; // Если дошли сюда, значит исключения не было
+            } catch (Exception e) {
+                System.out.println("Error detected: " + e.getMessage());
+                System.out.println("Please try again.");
+            }
+        }
+    }
+
+    public void printDetails(String key) throws Exception {
+        String message = getDetails(key);
+        System.out.println(message);
+    }
+
+    private String getDetails(String key) throws Exception {
+        if (key.equals("")) {
+            throw new Exception("Key set to empty string");
+        }
+        return "data for " + key;
+    }
+}
